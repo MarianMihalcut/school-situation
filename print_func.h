c6ecea9a28f1716f79653be08bd1ac97dc30e3d4
MarianMihalcut <marianmihalcut@gmail.com>
2024-08-12 21:46:45 +0300

    Written most of help functions for commands

---
+++ print_func.h
@@ -0,0 +1,21 @@
+#ifndef PRINT_FUNC_H_
+#define PRINT_FUNC_H
+
+//for listing all commands
+void help_menu();
+
+//for listing a specific command
+void help_add_data();
+void list_field();
+void list_table();
+void delete_field();
+void delete_all();
+
+//for calculations
+void calc_average_student();
+void calc_average_general();
+void calc_average_subject();
+void count_students_passed();
+void count_students_failed();
+
+#endif
