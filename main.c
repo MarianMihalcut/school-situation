552a8bf087d8e59ce89849e3fa68a3962418bb5b
MarianMihalcut <marianmihalcut@gmail.com>
2024-08-12 21:48:21 +0300

    Added new files, main.c modified

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+int main(int argc,char *argv[])
+{
+    for(int i=1;i<argc;i++)
+        printf("%s\n",argv[i]);
+    if(argc==1)
+        printf("Program code loaded. Please use argument commands to continue using the program.\n");
+    
+    return 0;
+}
